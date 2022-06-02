dc80267ba75d610bd7b03027bda49328d974562e
a-ouadid <103201524+a-ouadid@users.noreply.github.com>
2022-06-02 15:22:35 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@
 
 provider "aws" {
   region     = "eu-west-3"
-  access_key = "<##ACCESS_KEY##>"
-  secret_key = "<##SECRET_KEY##>"
+  access_key = "AKIA6AIDAZZFRU3JFU5U"
+  secret_key = "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"
 
 }
