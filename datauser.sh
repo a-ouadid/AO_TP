b299165a88724f1d1d11c5523330ca765d349e88
a-ouadid <103201524+a-ouadid@users.noreply.github.com>
2022-06-02 15:17:34 +0200

    Update datauser.sh

--- datauser.sh
+++ datauser.sh
@@ -3,3 +3,9 @@ yum update -y
 yum install -y httpd
 systemctl enable --now httpd
 cat /etc/hostname | tee /var/www/html/index.html
+I=0
+if  [ I eq  0] 
+echo "ittttt workkssssssss"
+else 
+echo " not today "  
+fi 
