4dd9554e2bf512dbb9f069d0c3135ca092282c93
a-ouadid <103201524+a-ouadid@users.noreply.github.com>
2022-06-02 14:43:20 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.12.1"
-    }
-  }
-}
+
 
 provider "aws" {
   region     = "eu-west-3"
-  access_key = "AKIA6AIDAZZFRU3JFU5U"
-  secret_key = "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"
+  access_key = "<##ACCESS_KEY##>"
+  secret_key = "<##SECRET_KEY##>"
 
 }
